7e75041081eb8ac2458c805658e84983402f921f
Jonathan Slate <jslate@patientslikeme.com>
2017-12-06 15:24:54 -0500

    adding more locations

--- src/Locations.js
+++ src/Locations.js
@@ -1,10 +1,17 @@
 const Locations = [
+
+  {
+    name: 'Adams - Mt. Greylock', id: 207,
+  },
   {
     name: 'Amherst',
     id: 213,
     dates: [
       { date: '12/05/2017', airQuality: 'moderate' },
     ],
+  },
+  {
+    name: 'Aquinnah', id: 818,
   },
   {
     name: 'Boston',
@@ -14,11 +21,59 @@ const Locations = [
     ],
   },
   {
+    name: 'Brockton', id: 755,
+  },
+  {
+    name: 'Cape Cod and Islands', id: 20,
+  },
+  {
+    name: 'Fairhaven', id: 208,
+  },
+  {
     name: 'Fall River',
     id: 289,
     dates: [
       { date: '12/05/2017', airQuality: 'hazardous' },
     ],
+  },
+  {
+    name: 'Greenfield', id: 747,
+  },
+  {
+    name: 'Haverhill', id: 308,
+  },
+  {
+    name: 'Lynn', id: 210,
+  },
+  {
+    name: 'Milton - Blue Hill Observatory', id: 209,
+  },
+  {
+    name: 'Newburyport', id: 211,
+  },
+  {
+    name: 'North Adams', id: 883,
+  },
+  {
+    name: 'Pittsfield', id: 326,
+  },
+  {
+    name: 'Springfield', id: 143,
+  },
+  {
+    name: 'Stow', id: 215,
+  },
+  {
+    name: 'Truro', id: 206,
+  },
+  {
+    name: 'Uxbridge', id: 784,
+  },
+  {
+    name: 'Ware - Quabbin Summit', id: 214,
+  },
+  {
+    name: 'Worcester', id: 165,
   },
 ];
 
